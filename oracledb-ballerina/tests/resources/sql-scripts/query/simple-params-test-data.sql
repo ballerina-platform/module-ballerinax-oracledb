2f4f29f5510d3b0f563261d5bbcb27f9962022b9
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2021-04-07 13:23:52 +0530

    Remove Unnecessary Sql Scripts

--- oracledb-ballerina/tests/resources/sql-scripts/query/simple-params-test-data.sql
+++ oracledb-ballerina/tests/resources/sql-scripts/query/simple-params-test-data.sql
@@ -1,129 +1,140 @@
-CREATE DATABASE IF NOT EXISTS QUERY_SIMPLE_PARAMS_DB;
+BEGIN
+EXECUTE IMMEDIATE 'DROP TABLE ' || 'GeneralQueryTable';
+EXCEPTION
+WHEN OTHERS THEN
+    IF SQLCODE != -942 THEN
+        RAISE;
+    END IF;
+END;
 
-USE QUERY_SIMPLE_PARAMS_DB;
+BEGIN
+EXECUTE IMMEDIATE 'DROP TABLE ' || 'NumericSimpleQueryTable';
+EXCEPTION
+WHEN OTHERS THEN
+    IF SQLCODE != -942 THEN
+        RAISE;
+    END IF;
+END;
 
-DROP TABLE IF EXISTS DataTable;
+BEGIN
+EXECUTE IMMEDIATE 'DROP TABLE ' || 'CharSimpleQueryTable';
+EXCEPTION
+WHEN OTHERS THEN
+    IF SQLCODE != -942 THEN
+        RAISE;
+    END IF;
+END;
 
-CREATE TABLE IF NOT EXISTS DataTable(
-  row_id       INTEGER,
-  int_type     INTEGER,
-  long_type    BIGINT,
-  float_type   FLOAT,
-  double_type  DOUBLE,
-  boolean_type BOOLEAN,
-  string_type  VARCHAR(50),
-  decimal_type DECIMAL(20, 2),
-  PRIMARY KEY (row_id)
+BEGIN
+EXECUTE IMMEDIATE 'DROP TABLE ' || 'AnsiSimpleQueryTable';
+EXCEPTION
+WHEN OTHERS THEN
+    IF SQLCODE != -942 THEN
+        RAISE;
+    END IF;
+END;
+
+BEGIN
+EXECUTE IMMEDIATE 'DROP TABLE ' || 'SqlDsSimpleQueryTable';
+EXCEPTION
+WHEN OTHERS THEN
+    IF SQLCODE != -942 THEN
+        RAISE;
+    END IF;
+END;
+
+BEGIN
+EXECUTE IMMEDIATE 'DROP TABLE ' || 'LobSimpleQueryTable';
+EXCEPTION
+WHEN OTHERS THEN
+    IF SQLCODE != -942 THEN
+        RAISE;
+    END IF;
+END;
+
+CREATE TABLE GeneralQueryTable(
+  id NUMBER,
+  col_number NUMBER,
+  col_varchar2 VARCHAR2(4000),
+  PRIMARY KEY (id)
 );
 
-INSERT INTO DataTable (row_id, int_type, long_type, float_type, double_type, boolean_type, string_type, decimal_type)
-  VALUES(1, 1, 9223372036854774807, 123.34, 2139095039, TRUE, 'Hello', 23.45);
+INSERT INTO GeneralQueryTable (id, col_number, col_varchar2) VALUES(1, -23.4, 'Hello world');
 
-INSERT INTO DataTable (row_id) VALUES (2);
-
-INSERT INTO DataTable (row_id, int_type, long_type, float_type, double_type, boolean_type, string_type, decimal_type)
-  VALUES(3, 1, 9372036854774807, 124.34, 29095039, false, '1', 25.45);
-
-DROP TABLE IF EXISTS ComplexTypes;
-
-CREATE TABLE IF NOT EXISTS ComplexTypes(
-  row_id         INTEGER NOT NULL,
-  tinyblob_type     TINYBLOB,
-  blob_type         BLOB,
-  mediumblob_type   MEDIUMBLOB,
-  longblob_type     LONGBLOB,
-  tinytext_type   TINYTEXT,
-  text_type       TEXT,
-  mediumtext_type MEDIUMTEXT,
-  longtext_type   LONGTEXT,
-  binary_type  BINARY(27),
-  var_binary_type VARBINARY(27),
-  PRIMARY KEY (row_id)
+CREATE TABLE NumericSimpleQueryTable(
+  id NUMBER, 
+  col_number NUMBER, 
+  col_float FLOAT, 
+  col_binary_float BINARY_FLOAT, 
+  col_binary_double BINARY_DOUBLE, 
+  PRIMARY KEY (id) 
 );
 
-INSERT INTO ComplexTypes (row_id, tinyblob_type, blob_type, mediumblob_type, longblob_type, tinytext_type, text_type,
-mediumtext_type, longtext_type, binary_type, var_binary_type) VALUES
-  (1, X'77736F322062616C6C6572696E6120626C6F6220746573742E', X'77736F322062616C6C6572696E6120626C6F6220746573742E',
-  X'77736F322062616C6C6572696E6120626C6F6220746573742E', X'77736F322062616C6C6572696E6120626C6F6220746573742E',
-  'very long text', 'very long text','very long text','very long text',
-  X'77736F322062616C6C6572696E612062696E61727920746573742E', X'77736F322062616C6C6572696E612062696E61727920746573742E');
+INSERT INTO NumericSimpleQueryTable (id, col_number, col_float, col_binary_float, col_binary_double)
+  VALUES(1, 1, 922.337, 123.34, 123.34);
 
-INSERT INTO ComplexTypes (row_id, tinyblob_type, blob_type, mediumblob_type, longblob_type, tinytext_type, text_type,
-mediumtext_type, longtext_type, binary_type, var_binary_type) VALUES
-  (2, null, null, null, null, null, null, null, null, null, null);
-
-DROP TABLE IF EXISTS NumericTypes;
-
-CREATE TABLE NumericTypes (
-   id INT AUTO_INCREMENT,
-   int_type INT NOT NULL,
-   bigint_type BIGINT NOT NULL,
-   smallint_type SMALLINT NOT NULL ,
-   mediumint_type MEDIUMINT NOT NULL ,
-   tinyint_type TINYINT NOT NULL ,
-   bit_type BIT NOT NULL ,
-   decimal_type DECIMAL(10,3) NOT NULL ,
-   numeric_type NUMERIC(10,3) NOT NULL ,
-   float_type FLOAT NOT NULL ,
-   real_type REAL NOT NULL ,
-   PRIMARY KEY (id)
+CREATE TABLE CharSimpleQueryTable(
+  id NUMBER,
+  col_varchar2  VARCHAR2(4000),
+  col_varchar  VARCHAR(4000),
+  col_nvarchar2 NVARCHAR2(2000),
+  col_char CHAR(2000),
+  col_nchar NCHAR(1000),
+  PRIMARY KEY(id)
 );
 
-INSERT INTO NumericTypes (id, int_type, bigint_type, smallint_type, mediumint_type, tinyint_type, bit_type, decimal_type, numeric_type,
-    float_type, real_type) VALUES (1, 2147483647, 9223372036854774807, 32767, 8388607, 127, 1, 1234.567, 1234.567, 1234.567,
-    1234.567);
+INSERT INTO CharSimpleQueryTable (id, col_varchar2, col_varchar, col_nvarchar2, col_char, col_nchar)
+  VALUES(1, 'Hello world', 'Hello world', 'Hello world', 'Hello world', 'Hello world');
+    
+CREATE TABLE AnsiSimpleQueryTable
+  id NUMBER,
+  col_character  CHARACTER(256),
+  col_character_var  CHARACTER VARYING(256),
 
-INSERT INTO NumericTypes (id, int_type, bigint_type, smallint_type, mediumint_type, tinyint_type, bit_type, decimal_type, numeric_type,
-    float_type, real_type) VALUES (2, 2147483647, 9223372036854774807, 32767, 8388607, 127, 1, 1234, 1234, 1234,
-    1234);
+  col_national_character NATIONAL CHARACTER(256),
+  col_national_char NATIONAL CHAR(256),
 
-DROP TABLE IF EXISTS DateTimeTypes;
+  col_national_character_var NATIONAL CHARACTER VARYING(256),
+  col_national_char_var NATIONAL CHAR VARYING(256),
+  col_nchar_var NCHAR VARYING(256),
 
-CREATE TABLE IF NOT EXISTS DateTimeTypes(
-  row_id         INTEGER NOT NULL,
-  date_type      DATE,
-  time_type      TIME,
-  timestamp_type TIMESTAMP,
-  datetime_type  DATETIME,
-  PRIMARY KEY (row_id)
+  col_numeric NUMERIC,
+  col_decimal DECIMAL,
+
+  col_integer INTEGER,
+  col_int INT,
+  col_smallint SMALLINT,
+
+  col_float FLOAT,
+  col_double_precision DOUBLE PRECISION,
+  col_real REAL,
+        
+  PRIMARY KEY(id)
 );
 
-INSERT INTO DateTimeTypes (row_id, date_type, time_type, datetime_type, timestamp_type) VALUES
-  (1,'2017-02-03', '11:35:45', '2017-02-03 11:53:00', '2017-02-03 11:53:00');
+INSERT INTO AnsiSimpleQueryTable(id, col_character, col_character_var, col_national_character,
+  col_national_char, col_national_character_var, col_national_char_var, col_nchar_var, col_numeric,
+  col_decimal, col_integer, col_int, col_smallint, col_float, col_double_precision, col_real) VALUES
+  (1, 'Hello world', 'Hello world', 'Hello world', 'Hello world', 'Hello world', 'Hello world',
+  'Hello world', 1234134, 1234134, 1234134, 1234134, 1234134, 1234.134, 1234.134, 1234.134);
 
-DROP TABLE IF EXISTS ENUMTable;
-
-CREATE TABLE ENUMTable (
-    id integer NOT NULL,
-    enum_type ENUM('admin','doctor','housekeeper') DEFAULT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE SqlDsSimpleQueryTable(
+  id NUMBER,
+  col_character  CHARACTER(255),
+  col_long_varchar  LONG VARCHAR,
+  PRIMARY KEY(id)
 );
 
-INSERT INTO ENUMTable(id, enum_type) VALUES (1, 'doctor');
+INSERT INTO SqlDsSimpleQueryTable(id, col_character, col_long_varchar)
+  VALUES (1, 'Hello world', 'Hello world');
 
-DROP TABLE IF EXISTS SetTable;
-
-CREATE TABLE SetTable (
-    row_id INTEGER NOT NULL,
-    set_type SET('a', 'b', 'c', 'd')
+CREATE TABLE LobSimpleQueryTable(
+  id NUMBER,
+  col_clob  CLOB, 
+  col_nclob  NCLOB, 
+  col_blob BLOB, 
+  PRIMARY KEY(id)
 );
 
-INSERT INTO SetTable (row_id, set_type) VALUES (1, 'a,d'), (2, 'd,a'), (3, 'a,d,a');
-
-DROP TABLE IF EXISTS GEOTable;
-
-CREATE TABLE GEOTable(
-    id INTEGER NOT NULL ,
-    geom GEOMETRY
-);
-
-INSERT INTO GEOTable (id, geom) values (1, ST_GeomFromText('POINT(7 52)'));
-
-DROP TABLE IF EXISTS JsonTable;
-
-CREATE TABLE JsonTable(
-    id INTEGER NOT NULL ,
-    json_type JSON
-);
-
-INSERT INTO JsonTable (id, json_type) values (1, JSON_OBJECT('id', 100, 'name', 'Joe', 'groups', '[2,5]'));
+INSERT INTO LobSimpleQueryTable(id, col_clob, col_nclob, col_blob)
+  VALUES (1, 'Hello world', 'Hello world', 'AB34EFC234');
